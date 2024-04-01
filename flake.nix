e07ee1f3498876b85ab842534881d744cbdca468
Jacob Birkett <jacob@birkett.dev>
2024-03-31 23:14:58 -0700

    flake: devShell: use rustfmt and RA from nightly

--- flake.nix
+++ flake.nix
@@ -21,28 +21,34 @@
       devShells = eachSystem (system:
         let
           pkgs = pkgsFor.${system};
-          rust-version = "latest";
-          rust-toolchain = pkgs.rust-bin.stable.${rust-version}.default.override
-            {
-              extensions = [
-                "rust-src"
-                "rust-analyzer"
-              ];
-            };
+          rust-stable = pkgs.rust-bin.stable.latest.minimal.override {
+            extensions = [ "rust-src" "rust-docs" "clippy" ];
+          };
         in
         {
-          default = with pkgs; mkShell {
-            strictDeps = true;
-            packages = [
-              openssl
-              pkg-config
-              rust-toolchain
-            ];
-            OPENSSL_LIB_DIR = "${openssl.out}/lib";
-            OPENSSL_ROOT_DIR = "${openssl.out}";
-            OPENSSL_INCLUDE_DIR = "${openssl.dev}/include";
-            # RUST_BACKTRACE = 1;
-          };
+          default = with pkgs;
+            mkShell {
+              strictDeps = true;
+              packages = [
+                # Derivations in `rust-stable` take precedence over nightly.
+                (lib.hiPrio rust-stable)
+
+                # Use rustfmt, and other tools that require nightly features.
+                (pkgs.rust-bin.selectLatestNightlyWith
+                  (toolchain:
+                    toolchain.minimal.override {
+                      extensions = [ "rustfmt" "rust-analyzer" ];
+                    }))
+
+                # Native transitive dependencies for Cargo
+                pkg-config
+                openssl
+              ];
+              OPENSSL_LIB_DIR = "${openssl.out}/lib";
+              OPENSSL_ROOT_DIR = "${openssl.out}";
+              OPENSSL_INCLUDE_DIR = "${openssl.dev}/include";
+              # RUST_BACKTRACE = 1;
+            };
         });
 
       formatter = eachSystem (system: pkgsFor.${system}.nixpkgs-fmt);
