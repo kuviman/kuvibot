00db7a42a22b5ef340d80dd9e9d0d9abbb493487
Jacob Birkett <jacob@birkett.dev>
2024-06-29 13:23:32 -0700

    flake: packages: init kuvibot and default
    flake: apps: kuvibot: inherit from packages

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
     systems = {
       url = "github:nix-systems/default";
       flake = false;
     };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -19,20 +19,27 @@
           overlays = [ rust-overlay.overlays.default ];
         });
     in {
-      apps = eachSystem (system:
+      apps = eachSystem (system: {
+        default = self.apps.${system}.kuvibot;
+        kuvibot = {
+          type = "app";
+          program = lib.getExe self.packages.${system}.kuvibot;
+        };
+      });
+
+      packages = eachSystem (system:
         let
           pkgs = pkgsFor.${system};
           craneLib = crane.mkLib pkgs;
+        in {
+          default = self.packages.${system}.kuvibot;
           kuvibot = craneLib.buildPackage {
             src = craneLib.cleanCargoSource ./.;
             buildInputs = with pkgs; [ pkg-config openssl ];
-          };
-        in {
-          default = {
-            type = "app";
-            program = "${kuvibot}/bin/kuvibot";
+            meta.mainProgram = "kuvibot";
           };
         });
+
       devShells = eachSystem (system:
         let
           pkgs = pkgsFor.${system};
